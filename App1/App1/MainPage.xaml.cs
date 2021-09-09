637a2b30b19345f8a2493a4a918b73dc44865fb4
Touseef Malik <Touseef.Malik@365labs.com>
2021-09-09 13:31:57 +0500

    added project

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using App1.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel { get; private set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewModel = new MainViewModel();
+        }
+    }
+}
---
+++ App1/App1/ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+
+namespace App1.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _locationName;
+        private string _subLocationName;
+
+
+        public MainViewModel() => PropertyChanged += MyClass_PropertyChanged;
+
+        private void MyClass_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(LocationName) || e.PropertyName == nameof(SubLocationName))
+            {
+                RaisePropertyChanged(LocationCompleteString);
+            }
+        }
+
+        public string LocationCompleteString => $"{LocationName}/{SubLocationName}";
+        public string LocationName
+        {
+            get => _locationName;
+            set
+            {
+                if (_locationName != value)
+                {
+                    _locationName = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public string SubLocationName
+        {
+            get => _subLocationName;
+            set
+            {
+                if (_subLocationName != value)
+                {
+                    _subLocationName = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
